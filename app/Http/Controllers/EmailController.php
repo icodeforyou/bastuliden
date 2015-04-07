e49d6897788073b9d58a799ec21df102f74c1b24
Johan Thorstedt <webdev@icode4u.se>
2015-04-07 19:26:57 +0200

    mail

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -71,6 +71,7 @@ class EmailController extends Controller {
 
 		$res = Mail::send("emails.newsletter", ["email" => $email->email_content], function ($message) use ($email) {
             $message->from("no-reply@oktorp.se");
+            $message->replyTo("fiber@oktorp.se");
             $message->to($email->recipients)->subject($email->subject);
         });
 
